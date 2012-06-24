093be7e3bc530f6366b7a8846040bf0e417a9b21
Andrea Campi <andrea.campi@zephirworks.com>
2012-06-24 11:47:01 +0200

    Convert attribute access to hash syntax; methods break when the attribute is undefined.

--- recipes/sinatra.rb
+++ recipes/sinatra.rb
@@ -3,7 +3,7 @@ template File.join(node.cloudfoundry_common.staging_manifests_dir, "sinatra.yml"
   owner  node.cloudfoundry_common.user
   mode   "0644"
   variables(
-    :ruby_1_8_7_runtime => node.cloudfoundry_dea.runtimes.ruby18,
-    :ruby_1_9_2_runtime => node.cloudfoundry_dea.runtimes.ruby19
+    :ruby_1_8_7_runtime => node['cloudfoundry_dea']['runtimes']['ruby18'],
+    :ruby_1_9_2_runtime => node['cloudfoundry_dea']['runtimes']['ruby19']
   )
 end
