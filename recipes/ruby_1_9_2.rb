b58b2c10a6fd6650b1674dc69c289957735ff0eb
Andrea Campi <andrea.campi@zephirworks.com>
2012-08-17 10:54:39 +0200

    FC019: Access node attributes in a consistent manner

--- recipes/ruby_1_9_2.rb
+++ recipes/ruby_1_9_2.rb
@@ -1,10 +1,10 @@
-node.default[:cloudfoundry_dea][:runtimes][:ruby19][:executable]   = File.join(node[:rbenv][:system_prefix], "rbenv", "versions", node[:cloudfoundry_ruby_runtime][:ruby_1_9_2_version], "bin", "ruby")
-node.default[:cloudfoundry_dea][:runtimes][:ruby19][:version]      = node[:cloudfoundry_ruby_runtime][:ruby_1_9_2_version].sub('-', '')
+node.default['cloudfoundry_dea']['runtimes']['ruby19']['executable']  = File.join(node['rbenv']['system_prefix'], "rbenv", "versions", node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'], "bin", "ruby")
+node.default['cloudfoundry_dea']['runtimes']['ruby19']['version']     = node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'].sub('-', '')
 
 include_recipe "rbenv"
 
-rbenv_ruby node.cloudfoundry_ruby_runtime.ruby_1_9_2_version
+rbenv_ruby node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version']
 
 rbenv_gem "bundler" do
-  ruby_version node.cloudfoundry_ruby_runtime.ruby_1_9_2_version
+  ruby_version node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version']
 end
