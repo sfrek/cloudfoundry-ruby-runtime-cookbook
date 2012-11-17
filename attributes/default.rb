dcbfa7030218e7d5f38b602010a73b2631d8acae
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-17 23:36:04 +0100

    Use the new cloudfoundry_runtime LWRP instead of manipulating attributes that belong to another cookbook.

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,10 +6,5 @@ default['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'] = "1.9.2-p290"
 # default['cloudfoundry_dea']['runtimes']['ruby18']['version_flag']= "-v | cut -d' ' -f2"
 # default['cloudfoundry_dea']['runtimes']['ruby18']['default']     = false
 
-default['cloudfoundry_dea']['runtimes']['ruby19']['name']        = "ruby19"
-default['cloudfoundry_dea']['runtimes']['ruby19']['cookbook']    = "cloudfoundry-ruby-runtime::ruby_1_9_2"
-default['cloudfoundry_dea']['runtimes']['ruby19']['version_flag']= "-v | cut -d' ' -f2"
-default['cloudfoundry_dea']['runtimes']['ruby19']['default']     = true
-
 default['cloudfoundry_cloud_controller']['server']['frameworks']['rails3']['cookbook']  = "cloudfoundry-ruby-runtime::rails3"
 default['cloudfoundry_cloud_controller']['server']['frameworks']['sinatra']['cookbook'] = "cloudfoundry-ruby-runtime::sinatra"
--- recipes/ruby_1_9_2.rb
+++ recipes/ruby_1_9_2.rb
@@ -1,11 +1,18 @@
-node.default['cloudfoundry_dea']['runtimes']['ruby19']['executable']  = ruby_bin_path(node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'])
-node.default['cloudfoundry_dea']['runtimes']['ruby19']['version']     = node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'].sub('-', '')
+ruby_ver  = node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version']
+ruby_exe  = ruby_bin_path(node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'])
 
 include_recipe "rbenv"
 include_recipe "rbenv::ruby_build"
 
-rbenv_ruby node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version']
+rbenv_ruby ruby_ver
 
 rbenv_gem "bundler" do
-  ruby_version node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version']
+  ruby_version ruby_ver
+end
+
+cloudfoundry_runtime "ruby19" do
+  version       ruby_ver.sub('-', '')
+  executable    ruby_exe
+  version_flag  "-v | cut -d' ' -f2"
+  default       true
 end
