7616f230b40b3a3a2558d63824e1209d9ab9eec7
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-18 00:26:01 +0100

    Cleanup: get rid of attributes that are actually getting in the way.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,2 @@
 # default['cloudfoundry_ruby_runtime']['ruby_1_8_7_version'] = "1.8.7-p334"
 default['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'] = "1.9.2-p290"
-
-# default['cloudfoundry_dea']['runtimes']['ruby18']['name']        = "ruby18"
-# default['cloudfoundry_dea']['runtimes']['ruby18']['cookbook']    = "cloudfoundry-ruby-runtime::ruby_1_8_7"
-# default['cloudfoundry_dea']['runtimes']['ruby18']['version_flag']= "-v | cut -d' ' -f2"
-# default['cloudfoundry_dea']['runtimes']['ruby18']['default']     = false
-
-default['cloudfoundry_cloud_controller']['server']['frameworks']['rails3']['cookbook']  = "cloudfoundry-ruby-runtime::rails3"
-default['cloudfoundry_cloud_controller']['server']['frameworks']['sinatra']['cookbook'] = "cloudfoundry-ruby-runtime::sinatra"
