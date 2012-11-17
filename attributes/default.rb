ed9bb341c5d3e25116e1913739707ddfcd36471a
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-17 12:11:00 +0100

    Make a computed attribute more readable by using a helper.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,3 @@
-include_attribute "rbenv"
-
 # default['cloudfoundry_ruby_runtime']['ruby_1_8_7_version'] = "1.8.7-p334"
 default['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'] = "1.9.2-p290"
 
--- recipes/ruby_1_9_2.rb
+++ recipes/ruby_1_9_2.rb
@@ -1,4 +1,4 @@
-node.default['cloudfoundry_dea']['runtimes']['ruby19']['executable']  = File.join(node['rbenv']['system_prefix'], "rbenv", "versions", node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'], "bin", "ruby")
+node.default['cloudfoundry_dea']['runtimes']['ruby19']['executable']  = ruby_bin_path(node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'])
 node.default['cloudfoundry_dea']['runtimes']['ruby19']['version']     = node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'].sub('-', '')
 
 include_recipe "rbenv"
