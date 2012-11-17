888018cfc659f11b26e990f0abf03fd31f166979
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-17 13:58:07 +0100

    Runtimes configuration has been moved to the cloud_controller, so it doesn't belong here anymore.

--- recipes/rails3.rb
+++
@@ -1,9 +0,0 @@
-template File.join(node['cloudfoundry']['staging_manifests_dir'], "rails3.yml") do
-  source "rails3.yml.erb"
-  owner  node['cloudfoundry']['user']
-  mode   "0644"
-  variables(
-    :ruby_1_8_7_runtime => node['cloudfoundry_dea']['runtimes']['ruby18'],
-    :ruby_1_9_2_runtime => node['cloudfoundry_dea']['runtimes']['ruby19']
-  )
-end
