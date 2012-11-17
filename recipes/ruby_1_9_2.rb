2d614415460365494f5f3c2823194f7a76ee0fbe
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-17 12:11:18 +0100

    We need to include rbenv::ruby_build.

--- recipes/ruby_1_9_2.rb
+++ recipes/ruby_1_9_2.rb
@@ -2,6 +2,7 @@ node.default['cloudfoundry_dea']['runtimes']['ruby19']['executable']  = ruby_bin
 node.default['cloudfoundry_dea']['runtimes']['ruby19']['version']     = node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'].sub('-', '')
 
 include_recipe "rbenv"
+include_recipe "rbenv::ruby_build"
 
 rbenv_ruby node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version']
 
