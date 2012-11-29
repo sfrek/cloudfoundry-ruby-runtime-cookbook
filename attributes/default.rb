b267d56848ab31c74e79f5b3dbd49d7bd524367e
Andrea Campi <andrea.campi@zephirworks.com>
2012-11-30 00:08:58 +0100

    Add missing copyright notices.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,23 @@
+#
+# Cookbook Name:: cloudfoundry-ruby-runtime
+# Attributes:: default
+#
+# Copyright 2012, ZephirWorks
+# Copyright 2012, Trotter Cashion
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 default['cloudfoundry_ruby_runtime']['ruby_1_8_7_version'] = "1.8.7-p334"
 default['cloudfoundry_ruby_runtime']['ruby_1_9_2_version'] = "1.9.2-p290"
 default['cloudfoundry_ruby_runtime']['bundler_version'] = "1.1.3"
--- recipes/ruby_1_8_7.rb
+++ recipes/ruby_1_8_7.rb
@@ -1,3 +1,23 @@
+#
+# Cookbook Name:: cloudfoundry-ruby-runtime
+# Recipe:: ruby_1_8_7
+#
+# Copyright 2012, ZephirWorks
+# Copyright 2012, Trotter Cashion
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 ruby_ver  = node['cloudfoundry_ruby_runtime']['ruby_1_8_7_version']
 ruby_path = ruby_bin_path(node['cloudfoundry_ruby_runtime']['ruby_1_8_7_version'])
 ruby_exe  = ::File.join(ruby_path, 'ruby')
