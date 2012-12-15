fb3e22b2a473ae26231102beb25b1a76f408cef3
Andrea Campi <andrea.campi@zephirworks.com>
2012-12-15 12:30:03 +0100

    Update tests to match the renamed recipes.

--- files/default/tests/minitest/ruby_1_9_2_test.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path('../support/helpers', __FILE__)
-
-describe 'cloudfoundry-ruby-runtimes::ruby_1_9_2' do
-  include Helpers::CloudfoundryRubyRuntime
-
-  it 'installs ruby 1.9.2 in rbenv' do
-    directory("/opt/rbenv/versions/#{node['cloudfoundry_ruby_runtime']['ruby_1_9_2_version']}").must_exist
-  end
-end
